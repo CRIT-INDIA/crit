b3359f12e98006cf529d821191f7f56eea0633e6
Crit <admin@critindia.com>
2025-07-10 12:32:11 +0530

    Fix ESLint errors for Vercel build-1

--- app/components/ui/ModalPortal.js
+++ app/components/ui/ModalPortal.js
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 export default function ModalPortal({ children }) {
+  
   const elRef = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement('div');
