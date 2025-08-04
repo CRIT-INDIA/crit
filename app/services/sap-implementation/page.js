e9644aca4808a7533f9c096766c610aaaca14f34
Crit <admin@critindia.com>
2025-08-04 12:05:10 +0530

    white theme-24

--- app/services/sap-implementation/page.js
+++ app/services/sap-implementation/page.js
@@ -1,12 +1,12 @@
 'use client';
 
-import ServiceBlock from '@/components/ui/ServiceBlock';
+import ServiceBlock from '../../components/ui/ServiceBlock';
 
 export default function SapImplementationPage() {
   return (
     <div className="relative bg-white min-h-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <ServiceBlock />
+        <ServiceBlock serviceName="SAP Implementation" />
       </div>
     </div>
   );
