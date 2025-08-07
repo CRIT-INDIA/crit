558136022b63fd8753ccbb189419e4b77b4aa7f6
Crit <admin@critindia.com>
2025-08-07 10:16:54 +0530

    commit

--- app/career/apply/page.js
+++ app/career/apply/page.js
@@ -1,9 +1,8 @@
-import CareerForm from '../components/career/CareerForm';
-
 export default function Page() {
-  return(
-  <div className='pt-20'>
-  <CareerForm />;
-  </div>
-    );
-} 
+  return (
+    <div className='pt-20 px-4'>
+      <h1 className="text-2xl font-bold text-center">Career Application</h1>
+      <p className="text-center mt-4">Application form coming soon.</p>
+    </div>
+  );
+}
